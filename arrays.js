72fdd6e947457b89c31cbf5be246ba91ba578fab
tr0b <60306480+tr0b@users.noreply.github.com>
2023-04-16 21:14:56 -0600

    Will add merge sort array solution to array file

--- arrays.js
+++ arrays.js
@@ -52,4 +52,26 @@ class MyArray {
     this.data = reverseArray.data;
     return this.data;
   }
+
+  function merge(arr1, arr2) {
+    let arr1Index = 0;
+    let arr2Index = 0;
+    const merged = [];
+    let array1Item = arr1[arr1Index];
+    let array2Item = arr2[arr2Index];
+    while (array1Item || array2Item) {
+      const shouldGrabArr1Item = !array2Item || array1Item < array2Item;
+      if (shouldGrabArr1Item) {
+        merged.push(array1Item);
+        arr1Index++;
+        array1Item = arr1[arr1Index];
+      } else {
+        merged.push(array2Item);
+        arr2Index++;
+        array2Item = arr2[arr2Index]
+
+      }
+    }
+    return merged;
+  }
 }
