a524cf7f20599e2f64c523228541ee6e5af96d96
tr0b <60306480+tr0b@users.noreply.github.com>
2023-04-16 20:19:42 -0600

    Will add array data structure, along with additional shift function

---
+++ arrays.js
@@ -0,0 +1,44 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index]
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return item;
+  }
+
+  pop() {
+    const item = this.data[this.length-1];
+    delete this.data[this.length-1];
+    this.length--;
+    return item;
+  }
+
+  shift() {
+    const item = this.data[0];
+    this.shiftItems(0);
+    return item;
+  }
+
+  remove(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length -1; i++) {
+      this.data[i] = this.data[i+1];
+    }
+
+    delete this.data[this.length-1];
+    this.length--;
+  }
+}
